6fb870252994f85a0a8180d2e87d68a8dbc68f27
skyhackeR <jolayemidavid6@gmail.com>
2024-07-11 17:17:12 +0100

    Use of column and row

--- lib/main.dart
+++ lib/main.dart
@@ -2,13 +2,45 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp( 
-    MaterialApp(
+    const MyApp()
+  );
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.teal,
-        body: Container(
-          
+        backgroundColor: Colors.black,
+        body: SafeArea(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget> [
+              Container(
+                height: 100.0,
+                width: 100.0, 
+                color: Colors.white,
+                child: const Text('Container 1'),
+              ),
+              Container(
+                width: 100.0,
+                height: 100.0,
+                color: Colors.brown,
+                child: Text('Container 2'),
+              ),
+              Container(
+                height: 100.0,
+                width: 100.0, 
+                color: Colors.yellow,
+                child: const Text('Container 3'),
+              ),
+            ],
+          ),
         ),
-      )
-    ));
+      ),
+    );
+  }
 }
 
