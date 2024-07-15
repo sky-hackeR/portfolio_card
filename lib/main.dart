2343bdb9422a3a0b9c9d5f6a8bc8872e46c7dd80
skyhackeR <jolayemidavid6@gmail.com>
2024-07-15 16:16:24 +0100

    Test

--- lib/main.dart
+++ lib/main.dart
@@ -25,9 +25,26 @@ class MyApp extends StatelessWidget {
               Text(
                 'Jolayemi Olugbnega David',
                 style: TextStyle(
+                  fontFamily: 'Pacifico',
                   fontSize: 40.0,
                   color: Colors.white,
                   fontWeight: FontWeight.bold
+                ),
+              ),
+              Text(
+                'JUNIOR SOFTWARE ENGINEER',
+                style: TextStyle(
+                  fontFamily: 'Source Sans 3',
+                  fontSize: 20.0,
+                  letterSpacing: 2.5,
+                  fontWeight: FontWeight.bold,
+                  color: Color.fromARGB(255, 152, 146, 146),
+                ),
+              ),
+              const Container(
+                child: Row(
+                  children: <Widget>[
+                    Icon(Icons.add_shopping_cart)],
                 ),
               ),
             ],
