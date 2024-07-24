bc7a7145e887d9ee55c49d4e123004b8788bf7fc
skyhackeR <jolayemidavid6@gmail.com>
2024-07-24 14:13:05 +0100

    Completed MiCard

--- lib/main.dart
+++ lib/main.dart
@@ -11,82 +11,85 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return  MaterialApp(
+    return  const MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.black,
         body: SafeArea(
           child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
-              const CircleAvatar(
+              CircleAvatar(
                 radius: 50.0,
                 backgroundColor: Colors.brown,
                 backgroundImage: AssetImage('images/dave.jpg'),
               ),
-              const Text(
-                'Jolayemi Olugbnega David',
+              Text(
+                'Jolayemi Olugbenga David',
                 style: TextStyle(
                   fontFamily: 'Pacifico',
-                  fontSize: 40.0,
+                  fontSize: 30.0,
                   color: Colors.white,
                   fontWeight: FontWeight.bold
                 ),
               ),
-              const Text(
+
+              SizedBox(
+                height: 10.0,
+              ),
+
+              Text(
                 'JUNIOR SOFTWARE ENGINEER',
                 style: TextStyle(
                   fontFamily: 'Source Sans 3',
-                  fontSize: 20.0,
+                  fontSize: 15.0,
                   letterSpacing: 2.5,
                   fontWeight: FontWeight.bold,
                   color: Color.fromARGB(255, 152, 146, 146),
                 ),
               ),
-              Container(
-                padding: const EdgeInsets.all(10.0),
-                color: Colors.white,
-                margin: const EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
-                child: const Row(
-                  children: <Widget>[
-                    Icon(
-                      Icons.phone,
-                      color: Colors.black,
-                    ),
-                    SizedBox(
-                      width: 10.0,
-                    ),
-                    Text('+234 8160 998 844',
-                    style: TextStyle(
-                      color: Colors.black,
-                      fontFamily: 'Source Sans 3',
-                      fontSize: 20.0,
-                    ),
-                    ),
-                    ],
+
+              SizedBox(
+                height: 20.0,
+                width: 200.0,
+                child: Divider(
+                  color: Colors.white,
                 ),
               ),
 
-              Container(
-                padding: const EdgeInsets.all(10.0),
+              Card(
                 color: Colors.white,
-                margin: const EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
-                child: const Row(
-                  children: <Widget>[
-                    Icon(
-                      Icons.mail,
-                      color: Colors.black,
-                    ),
-                    SizedBox(
-                      width: 10.0,
-                    ),
-                    Text('jolayemidavid6@gmail.com',
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                child: ListTile(
+                  leading:Icon(
+                    Icons.phone,
+                    color: Colors.black,
+                  ),
+                  title: Text('+234 8160 998 844',
                     style: TextStyle(
                       color: Colors.black,
                       fontFamily: 'Source Sans 3',
                       fontSize: 20.0,
                     ),
                     ),
-                    ],
-                ),
+                )
+              ),
+
+              Card(
+                color: Colors.white,
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                child: ListTile(
+                  leading:Icon(
+                    Icons.mail,
+                    color: Colors.black,
+                  ),
+                  title: Text('jolayemidavid6@gmail.com',
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontFamily: 'Source Sans 3',
+                      fontSize: 20.0,
+                    ),
+                    ),
+                )
               ),
             ],
           ),
